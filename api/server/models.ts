e0c530c1f627355a32a25d7fc54988783e9d0801
vic_june <victor@ileotech.com>
2018-01-24 16:59:21 +0100

    Add collections

---
+++ api/server/models.ts
@@ -0,0 +1,14 @@
+export interface Chat {
+  _id?: string;
+  title?: string;
+  picture?: string;
+  lastMessage?: Message;
+}
+
+export interface Message {
+  _id?: string;
+  chatId?: string;
+  content?: string;
+  createdAt?: Date;
+  type?: string
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chats, Messages } from '../../api/server/collections';
+import { Observable } from 'rxjs/Rx';
+import * as moment from 'moment';
+import { Chat } from 'api/server/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  chats;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.chats = Chats.find({})
+      .mergeMap((chats: Chat[]) => Observable.combineLatest(...chats.map((chat: Chat) => Messages
+        .find({ chatId: chat._id })
+        .startWith(null)
+        .map(messages => {
+          if (messages) chat.lastMessage = messages[0];
+          return chat;
+        })))
+      ).zone();
+  }
+
+  removeChat(chat: Chat): void {
+    Chats.remove({_id: chat._id}).subscribe(() => {});
+  }
 }
